8d8e4d8aa142c5bc3d144bde2220b5cc0d7f8fa4
Arwin Marinta <arwinmarinta3@gmail.com>
2023-08-26 04:07:33 +0800

    final push

--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@ const hamburger = document.querySelector("#hamburger");
 const navMobile = document.querySelector("#nav-mobile");
 const exit = document.querySelector("#exit");
 
-// Button Membuka Sidebar
+// Button Open Sidebar
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMobile.classList.toggle("hidden");
@@ -12,33 +12,3 @@ hamburger.addEventListener("click", function () {
 exit.addEventListener("click", function () {
   navMobile.classList.toggle("hidden");
 });
-
-// Navbar
-// window.onscroll = function () {
-//   const header = document.querySelector("navbar");
-//   const fixedNav = header.offsetTop;
-
-//   if (window.pageYOffset > fixedNav) {
-//     header.classList.add("navbar-fixed");
-//   } else {
-//     header.classList.remove("navbar-fixed");
-//   }
-// };
-
-// Slide Testimoni
-// let sliderContainer = document.getElementById("slideContainer");
-// let slider = document.getElementById("slider");
-// let card = slider.getElementById("li");
-
-// let elementsToShow = 2;
-
-// let slideContainerWidth = sliderContainer.clientWidth;
-
-// let cardWidth = slideContainerWidth / elementsToShow;
-
-// slider.style.width = card.length * cardWidth + "px";
-
-// for (let index = 0; index < card.length; index++) {
-//   const element = card[index];
-//   element.style.width = cardWidth + "px";
-// }
